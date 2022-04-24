b7269d8ff5e63cecf232086c646d3dbb6991be71
Bruce Dickson <95632543+BruceDicksonn@users.noreply.github.com>
2022-04-24 19:06:19 -0300

    Desafio javascript #21 - sets retirado do perfil do Fabio Bergmann

---
+++ set.js
@@ -0,0 +1,14 @@
+function myFunction(a,b){
+    
+    for(let value of b){
+        a.add(value);
+    }
+
+    return a;
+}
+
+console.log(myFunction(new Set('123'),new Set('234')));
+console.log(myFunction(new Set([1,2,3]),new Set([3,4,5])));
+console.log(
+    myFunction(new Set([false, false, NaN]), new Set([true, true, NaN]))
+);
